d9577dd3856bfd9b9c87c27fccee5f7873648ed6
Han Ngo <hankngo@Hans-MBP.hsd1.ca.comcast.net>
2024-11-04 22:49:16 -0800

    finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(smaller, larger) {
+    if (!Number.isInteger(smaller) || !Number.isInteger(larger)
+        || smaller < 0 || larger < 0) return "ERROR";
+    let sum = 0;
+    if (smaller > larger) {
+        let temp = larger;
+        larger = smaller;
+        smaller = temp;
+    }
 
+    for (let i = smaller; i <= larger; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
