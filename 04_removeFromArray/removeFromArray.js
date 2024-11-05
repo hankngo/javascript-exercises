c3c2d5c101a1aaf22ae6e87140265baca6762d2e
Han Ngo <hankngo@Hans-MBP.hsd1.ca.comcast.net>
2024-11-04 22:28:07 -0800

    finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let arr = args[0];
+    let itemsToRemove = args.slice(1);
+    return arr.filter(item => !itemsToRemove.includes(item));
 };
 
 // Do not edit below this line
