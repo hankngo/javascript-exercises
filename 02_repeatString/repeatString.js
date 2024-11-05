6e671dd74fefa346fd466dcfbcc88e6ed675043e
Han Ngo <hankngo@Hans-MBP.hsd1.ca.comcast.net>
2024-11-04 22:01:34 -0800

    finish 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeatNum) {
+    if (repeatNum < 0) return "ERROR";
+    let result = "";
+    for (let i = 0; i < repeatNum; i++) {
+        result += str;
+    }
+    return result;
 };
 
 // Do not edit below this line
